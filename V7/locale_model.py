bd1e7751615cc413b146a7b86ed41e3d55912d73
Özgür Yurt <93544343+ozguryurt@users.noreply.github.com>
2025-04-29 17:17:50 +0300

    Update locale_model.py

--- V7/locale_model.py
+++ V7/locale_model.py
@@ -1,9 +1,17 @@
-from transformers import pipeline
+from transformers import AutoTokenizer, pipeline, AutoModelForQuestionAnswering
 from sentence_transformers import SentenceTransformer
 
-# Huggingface modelini diske indir
-qa_pipeline = pipeline("question-answering", model="savasy/bert-base-turkish-squad")
-qa_pipeline.save_pretrained("./models/qa_model")
+# QA modeli ve tokenizer'ı indir ve kaydet
+model_name = "savasy/bert-base-turkish-squad"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+
+# Pipeline oluştur
+qa_pipeline = pipeline("question-answering", model=model, tokenizer=tokenizer)
+
+# Model ve tokenizer'ı kaydet
+model.save_pretrained("./models/qa_model")
+tokenizer.save_pretrained("./models/qa_model")
 
 # SentenceTransformer modelini indir ve kaydet
 embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
